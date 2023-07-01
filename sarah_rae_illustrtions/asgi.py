e3bdfc04a029a88d8f574928290890958f1d86af
Mike Rae <mikerae@me.com>
2023-07-01 14:03:10 +0000

    Add django project sarah_rae_illustrtions

---
+++ sarah_rae_illustrtions/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sarah_rae_illustrtions project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarah_rae_illustrtions.settings')
+
+application = get_asgi_application()
