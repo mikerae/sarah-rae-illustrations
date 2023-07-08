3d95d25ef284c11203a0eed5856560f8e9336adc
Mike Rae <mikerae@me.com>
2023-07-08 13:37:26 +0000

    Add bespoke nav for Shop and Gallery

--- digital_artworks/views.py
+++ digital_artworks/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def digital_artworks(request):
-    """ Return commissions landing  page view.
+    """ Render Digital Art works landing  page view with digital art works
+    path as context.
     """
+    site_area = request.path
+    context = {
+        'site_area': site_area,
+    }
 
-    return render(request, 'digital_artworks/digital_artworks.html')
+    return render(request, 'digital_artworks/digital_artworks.html', context)
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def shop(request):
-    """ Return shop landing  page view.
+    """ Render shop landing  page view with shop path as context.
     """
+    site_area = request.path
+    context = {
+        'site_area': site_area,
+    }
 
-    return render(request, 'shop/shop.html')
+    return render(request, 'shop/shop.html', context)
