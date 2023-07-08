618a029de0377acf2e1fadb350761efe11d2517f
Mike Rae <mikerae@me.com>
2023-07-08 16:28:55 +0000

    Add Product, Category Subcategory models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=245)
+    friendly_name = models.CharField(max_length=245, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=245)
+    friendly_name = models.CharField(max_length=245, null=True, blank=True)
+    category = models.ForeignKey('Category',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6,
+                                decimal_places=2,
+                                null=True,
+                                blank=True)
+    description = models.TextField()
+    image_url = models.ImageField(null=True, blank=True)
+    likes = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey('Category',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
