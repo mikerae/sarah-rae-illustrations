7d3145e783b5c737be212564fb9f604a08a2e54d
Mike Rae <mikerae@me.com>
2023-07-09 19:00:20 +0000

    Add products to Shop view

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def shop(request):
     """ Render shop landing  page view with shop path as context.
+        Show all products, including sorting and searching.
     """
     site_area = request.path
+    products = Product.objects.all()
+
     context = {
         'site_area': site_area,
+        'products': products,
     }
 
     return render(request, 'shop/shop.html', context)
