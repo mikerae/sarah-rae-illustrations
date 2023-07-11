da46a364848833b7eaf567afa28b18338d3d6c0b
Mike Rae <mikerae@me.com>
2023-07-11 12:25:51 +0000

    Add digital artworks detail view, url, template

--- digital_artworks/urls.py
+++ digital_artworks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.digital_artworks, name='digital_artworks')
+    path('', views.digital_artworks, name='digital_artworks'),
+    path('<digital_artwork_id>',
+         views.digital_artwork_detail, name='digital_artwork_detail')
 ]
--- digital_artworks/views.py
+++ digital_artworks/views.py
@@ -16,3 +16,20 @@ def digital_artworks(request):
     }
 
     return render(request, 'digital_artworks/digital_artworks.html', context)
+
+
+def digital_artwork_detail(request, digital_artwork_id):
+    """
+    Render digital artwork detail  page view with gallery path as context.
+    Show product digital artwork for selected product.
+    """
+    site_area = request.path
+    digital_artwork = get_object_or_404(DigitalArtWork, pk=digital_artwork_id)
+
+    context = {
+        'site_area': site_area,
+        'digital_artwork': digital_artwork,
+    }
+
+    return render(request,
+                  'digital_artworks/digital_artwork_detail.html', context)
