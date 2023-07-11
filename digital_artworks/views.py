be94948ce26584ca0bfae9228f03c5981963d063
Mike Rae <mikerae@me.com>
2023-07-11 10:55:10 +0000

    Add digital_artworks view and template

--- digital_artworks/views.py
+++ digital_artworks/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import DigitalArtWork
 
 
 def digital_artworks(request):
-    """ Render Digital Art works landing  page view with digital art works
-    path as context.
+    """ Render Digital Artworks landing  page view with gallery path as
+        context.
+        Show all Digital Artworks, including sorting and searching.
     """
     site_area = request.path
+    digital_artworks = DigitalArtWork.objects.all()
+
     context = {
         'site_area': site_area,
+        'digital_artworks': digital_artworks,
     }
 
     return render(request, 'digital_artworks/digital_artworks.html', context)
