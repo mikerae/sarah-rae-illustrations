fb7c46ce46cc30d6df9c6cafe3bc9f6bf1481846
Mike Rae <mikerae@me.com>
2023-07-12 19:44:57 +0000

    Add context.p to cart app

---
+++ cart/contexts.py
@@ -0,0 +1,15 @@
+
+
+def cart_contents(requests):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    centext = {
+        'cart_items' = cart_items,
+        'total' = total,
+        'product_count' = product_count,
+    }
+
+    return context
