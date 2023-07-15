7860a85081f4706065daa0acd2be6e65d82c87f4
Mike Rae <mikerae@me.com>
2023-07-15 21:46:43 +0000

    Add stripe element and styling

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,42 @@
+/*
+    Core logic/payment flow comes from this part of Stripe's docs (072023):
+    https://stripe.com/docs/payments/accept-a-payment
+
+    JS element Styling comes form here (072023):
+    https://stripe.com/docs/js/appendix/style?type=card
+
+    CSS Element container Styling is described here (072023)
+    https://stripe.com/docs/js/element/the_element_container?type=card
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var id_client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        iconColor: '#4d005299',
+        color: '#4d0052d9',
+        fontWeight: '500',
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+            color: '#4d0052d9',
+        },
+        '::placeholder': {
+            color: '#4d005299',
+        },
+    },
+    invalid: {
+        iconColor: '#dc3545',
+        color: '#dc3545',
+        fontSize: '19px',
+    },
+}
+var card = elements.create('card', {
+    style: style
+});
+
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NUDuCLQavTTy4OO03tEmLca6UilHp4doQh2Oaqz0QO5sPkeJQB6cEBsVLYxXyW9ld8IwV0bQAXG6kKBAF0WWYaS00iaYW3E6Q',  # noqa E501
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
