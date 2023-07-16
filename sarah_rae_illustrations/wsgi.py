96d527c75d8a0a09454509415bd87ec7bdee8849
Mike Rae <mikerae@me.com>
2023-07-16 14:06:39 +0000

    Change project name in commint in wsgi.py

--- sarah_rae_illustrations/wsgi.py
+++ sarah_rae_illustrations/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sarah_rae_illustrtions project.
+WSGI config for sarah_rae_illustrations project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
