a6fa60f0db895461ea4aca15db776a3a40def6db
Mike Rae <mikerae@me.com>
2023-07-16 13:38:46 +0000

    Corrected spelling mistake in main app!

--- sarah_rae_illustrations/asgi.py
+++ sarah_rae_illustrations/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sarah_rae_illustrtions project.
+ASGI config for sarah_rae_illustrations project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarah_rae_illustrtions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarah_rae_illustrations.settings')  # noqa E501
 
 application = get_asgi_application()
--- sarah_rae_illustrations/urls.py
+++ sarah_rae_illustrations/urls.py
@@ -1,4 +1,4 @@
-"""sarah_rae_illustrtions URL Configuration
+"""sarah_rae_illustrations URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
