d5f3e1e37e5f1c913a06301fc7cba49568595349
Mike Rae <mikerae@me.com>
2023-07-20 14:43:08 +0000

    Change Order required fields to not-required: County, phone_nmber, make postcode requirede- to match Stripe data input

--- checkout/models.py
+++ checkout/models.py
@@ -15,13 +15,13 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False)
     date = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     street_address_1 = models.CharField(max_length=80, null=False, blank=False)
     street_address_2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
     country = models.CharField(max_length=80, null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
     stripe_pid = models.CharField(max_length=254, null=False, blank=False)
     order_total = models.DecimalField(max_digits=10,
                                       decimal_places=2,
