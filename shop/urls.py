dd929063767708212a51068072d2aba41889d077
Mike Rae <mikerae@me.com>
2023-07-22 21:03:03 +0000

    Product Admi - Editing Products

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.shop, name='shop'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
     path('<int:product_id>/',
          views.product_detail, name='product_detail'),
 ]
