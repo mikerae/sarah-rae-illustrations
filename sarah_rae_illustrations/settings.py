65787cbb66ba27506844a830e3a59a7430c046f7
Mike Rae <mikerae@me.com>
2023-07-23 22:31:57 +0000

    Email does not work

--- sarah_rae_illustrations/settings.py
+++ sarah_rae_illustrations/settings.py
@@ -36,30 +36,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '8000-mikerae-sarahraeillustr-pno90mcmpvs.ws-eu102.gitpod.io',
 ]
-
-# Stripe
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_APP_WEBHOOK_SIGNING_SECRET = os.getenv('STRIPE_APP_WEBHOOK_SIGNING_SECRET', '')  # noqa E501
-STRIPE_CURRENCY = 'gbp'
-
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'sarah_rae_illustrations@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmailcom'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_DEPLOYED HOST')
     
-
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -234,4 +211,22 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# Stripe
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_APP_WEBHOOK_SIGNING_SECRET = os.getenv('STRIPE_APP_WEBHOOK_SIGNING_SECRET', '')  # noqa E501
+STRIPE_CURRENCY = 'gbp'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'sarah_rae_illustrations@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
