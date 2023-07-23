2aa045e43722975980608305a29d0b1743559e51
Mike Rae <mikerae@me.com>
2023-07-23 23:05:59 +0000

    Add 404page, Email now works

---
+++ sarah_rae_illustrations/views.py
@@ -0,0 +1,7 @@
+""" View for A custom 404 Page"""
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
