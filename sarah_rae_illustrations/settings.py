dd78b335fddf2ac12992c2ffe1ebe8151c2847b1
Mike Rae <mikerae@me.com>
2023-07-23 17:10:36 +0000

    fix AWS_S3_CUSTOM_DOMAIN

--- sarah_rae_illustrations/settings.py
+++ sarah_rae_illustrations/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAINid = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
