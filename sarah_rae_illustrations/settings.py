97ff87efe87625b89013a82eec4bca17f6076abe
Mike Rae <mikerae@me.com>
2023-07-23 14:31:04 +0000

    Remove sevret key and set debug

--- sarah_rae_illustrations/settings.py
+++ sarah_rae_illustrations/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7g0dyqspjo=emnugcc68td)w!s3&o$rd9g8$wtzsy(z_^r6!)!'  # noqa E501
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'sarah-rae-illustrations-d721ea03fb61.herokuapp.com',
